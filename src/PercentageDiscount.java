c1922836113c4e9a4fb1712b88b65e322280e688
DragonicOverload <ratchaphuek.i@ku.th>
2026-08-27 18:56:15 +0700

    Add files via upload

--- src/PercentageDiscount.java
+++ src/PercentageDiscount.java
@@ -10,8 +10,6 @@ public class PercentageDiscount implements DiscountStrategy {
 
     @Override public double applyDiscount(Order order) {
         double total = order.getTotalPrice();
-        // TODO (2a): คืนราคาหลังหักส่วนลด percent%
-        //   hint: total - (total * percent / 100.0)
-        return /* ====== replace this ====== */ total;
+        return total - (total * percent / 100.0); 
     }
 }
